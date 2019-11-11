fb773bd89976c3b01c226d3167f2607d21ab26a8
Michael Wurm <wurm.michael95@gmail.com>
2019-11-11 22:36:25 +0100

    adapt env init script

--- drivers/initbuildenv.sh
+++ drivers/initbuildenv.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-source /opt/poky/2.6.4/environment-setup-cortexa9hf-neon-poky-linux-gnueabi
+#####
+# NOTE: This file needs to be called like "source initbuildenv.sh"
+#####
 
+unset LD_LIBRARY_PATH
 export KERNEL_SRC=/opt/poky/2.6.4/sysroots/cortexa9hf-neon-poky-linux-gnueabi/usr/src/kernel/
 export LDFLAGS=""
+
+source /opt/poky/2.6.4/environment-setup-cortexa9hf-neon-poky-linux-gnueabi
 
 #export DEPLOYSSH=root@cyclone5
 #export DEPLOYSSHPATH=/home/root/
