05615c8764d1d4371e1fef515ce1312bbb96884a
Michael Wurm <wurm.michael95@gmail.com>
2019-11-11 22:21:17 +0100

    add makefile, empty sources, env init script

---
+++ drivers/initbuildenv.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source /opt/poky/2.6.4/environment-setup-cortexa9hf-neon-poky-linux-gnueabi
+
+export KERNEL_SRC=/opt/poky/2.6.4/sysroots/cortexa9hf-neon-poky-linux-gnueabi/usr/src/kernel/
+export LDFLAGS=""
+
+#export DEPLOYSSH=root@cyclone5
+#export DEPLOYSSHPATH=/home/root/
