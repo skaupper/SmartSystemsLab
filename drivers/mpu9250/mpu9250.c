17fa50ea271aafa0b5793bca6a636d154f78b3fb
Michael Wurm <wurm.michael95@gmail.com>
2019-12-29 16:51:48 +0100

    read status register

--- drivers/mpu9250/mpu9250.c
+++ drivers/mpu9250/mpu9250.c
@@ -307,7 +307,28 @@ static irqreturn_t irq_handler(int nr, void *data_ptr)
 
 static irqreturn_t irq_handler(int nr, void *data_ptr)
 {
+  struct data *dev = data_ptr;
+  uint32_t irqs;
   pr_info("Interrupt occured\n");
+
+  /* Determine which interrupt occured */
+  irqs = ioread32(dev->regs + MEM_OFFSET_BUF_ISR);
+
+  if (irqs == 0x1)
+  {
+    pr_info("Received Button[1] interrupt");
+    return IRQ_HANDLED;
+  }
+  else if (irqs == 0x2)
+  {
+    pr_info("Received Button[2] interrupt");
+    return IRQ_HANDLED;
+  }
+  else if (irqs == 0x3)
+  {
+    pr_info("Received Button[1] and Button[2] interrupt");
+    return IRQ_HANDLED;
+  }
   return IRQ_NONE;
 }
 
