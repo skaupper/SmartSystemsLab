c271f617ef416bab172bdec31d58bfa0aada2535
Michael Wurm <wurm.michael95@gmail.com>
2019-12-29 18:14:34 +0100

    adapt logic

--- hdl/components/button_irq/button_irq.vhd
+++ hdl/components/button_irq/button_irq.vhd
@@ -17,7 +17,7 @@ entity button_irq is
       button_i           : in   std_ulogic_vector(1 downto 0);
       interrupt_o        : out  std_logic;
 
-      avs_s0_address     : in  std_logic_vector( 1 downto 0);
+      avs_s0_address     : in  std_logic_vector(1 downto 0);
       avs_s0_read        : in  std_logic;
       avs_s0_readdata    : out std_logic_vector(31 downto 0);
       avs_s0_write       : in  std_logic;
@@ -36,18 +36,18 @@ architecture rtl of button_irq is
   constant cTimestampWidth : natural := 64;
   subtype timestamp_t is unsigned (cTimestampWidth-1 downto 0);
 
-
   type aRegSet is record
-    readdata  : std_ulogic_vector(avs_s0_readdata'range);
+    readdata  : std_ulogic_vector(avs_s0_readdata'range),
+    timestamp : timestamp_t;
   end record;
 
   constant cRegSetClear : aRegSet := (
-    readdata  => (others => '0'));
+    readdata => (others => '0'),
+    timestamp => (others => '0'));
 
-  signal timestamp : timestamp_t;
   signal msTick    : std_ulogic;
-
   signal readdata  : std_ulogic_vector(avs_s0_readdata'range);
+
   signal reg, nxR : aRegSet;
 
 begin -- architecture rtl
@@ -57,51 +57,48 @@ begin -- architecture rtl
     gClkFrequency    => gClkFrequency,
     gStrobeFrequency => cMsFrequency)
   port map (
-    clk_i             => clk_i,
+    clk_i            => clk_i,
     inResetAsync     => rst_i,
     oStrobe          => msTick);
 
-   comb : process(reg, avs_s0_read, avs_s0_address, avm_m0_readdata, avm_m0_waitrequest)
+   comb : process(reg, avs_s0_read, avs_s0_address)
    begin
       -- Defaults
-      nxR           <= reg;
-      nxR.readdata  <= (others => '0');
+      nxR          <= reg;
+      nxR.readdata <= (others => '0');
+
+      -- Timestamp
+      if msTick = '1' then
+        nxR.timestamp <= reg.timestamp + 1;
+      end if;
 
       -- Read access bus logic
       if avs_s0_read = '1' then
-         case( avs_s0_address ) is
-            when cAddrData =>
-               nxR.readdata(15 downto 0) <= reg.reg.light;
-               nxR.lock     <= '1';
-            when cAddrTsLo =>
-               nxR.readdata <= std_ulogic_vector(timestamp(31 downto 0));
-               nxR.lock     <= '1';
-            when cAddrTsUp =>
-               nxR.readdata <= std_ulogic_vector(timestamp(63 downto 32));
-               nxR.lock     <= '0';
-            when others =>
-               null;
-         end case ;
+        case (avs_s0_address) is
+          when cAddrData =>
+              nxR.readdata(buton_i'range) <= button_i;
+          when cAddrTsLo =>
+              nxR.readdata <= std_ulogic_vector(timestamp(31 downto 0));
+          when cAddrTsUp =>
+              nxR.readdata <= std_ulogic_vector(timestamp(63 downto 32));
+          when others =>
+              null;
+        end case;
       end if;
    end process comb;
 
    regs : process(clk_i, rst_i)
    begin
-    if (rising_edge(clk_i)) then
+    if rising_edge(clk_i) then
       if rst_i = '0' then
-        timestamp <= 0;
-        readdata <= (others => '0');
-
         reg <= cRegSetClear;
-        reg <= nxR;
       else
-        if msTick = '1' then
-          timestamp <= timestamp + 1;
-        end if;
+        reg <= nxR;
       end if;
     end if;
    end process regs;
 
-   avs_s0_readdata  <= std_logic_vector(readdata);
+   interrupt_o <= button_i(0) or button_i(1);
+   avs_s0_readdata <= std_logic_vector(reg.readdata);
 
 end architecture rtl; -- of button_irq
