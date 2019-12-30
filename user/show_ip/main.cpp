2fc511edca17ad314a007abd9631d668e6b8b241
Michael Wurm <wurm.michael95@gmail.com>
2019-12-30 16:06:51 +0100

    how come this f***ing thing doesn't work......

--- user/show_ip/main.cpp
+++ user/show_ip/main.cpp
@@ -21,7 +21,10 @@ struct IPv4Address {
 
 int writeToCDev(char chars[], uint8_t brightness) {
     static const int SEGMENT_COUNT = 6;
-    static const std::string CHARACTER_DEVICE = "/dev/sevensegment";
+//    static const std::string CHARACTER_DEVICE = "/dev/sevensegment";
+    static const std::string CHARACTER_DEVICE = "/dev/xxxxx";
+
+    std::cout << "here 11" << std::endl;
 
     // prepare input values
     uint8_t values[SEGMENT_COUNT];
@@ -40,6 +43,7 @@ int writeToCDev(char chars[], uint8_t brightness) {
         // Convert to binary for driver
         values[i] -= '0';
     }
+    std::cout << "here 12" << CHARACTER_DEVICE << std::endl;
 
     // open character device
     auto fd = fopen(CHARACTER_DEVICE.c_str(), "wb");
@@ -47,20 +51,23 @@ int writeToCDev(char chars[], uint8_t brightness) {
         std::cerr << "Failed to open character device '" << CHARACTER_DEVICE << "'" << std::endl;
         return -1;
     }
+    std::cout << "here 13" << std::endl;
 
     // write display values
     for (int i = 0; i < SEGMENT_COUNT; ++i) {
-        if (fputc(values[i], fd) == EOF) {
+        if (fputc(values[i] - '0', fd) == EOF) {
             std::cerr << "Failed to write character (index " << i << ")" << std::endl;
             return -1;
         }
     }
+    std::cout << "here 14" << std::endl;
 
     // write brightness level
     if (fputc(brightness, fd) == EOF) {
         std::cerr << "Failed to write brightness level" << std::endl;
         return -1;
     }
+    std::cout << "here 15" << std::endl;
 
     // write enable bits
     if (fputc(combinedEnabled, fd) == EOF) {
@@ -68,8 +75,15 @@ int writeToCDev(char chars[], uint8_t brightness) {
         return -1;
     }
 
-    (void) fclose(fd);
+    std::cout << "here 16" << std::endl;
 
+    auto ret = fclose(fd);
+    if (ret != 0) {
+        std::cerr << "error in fclose(fd): "<< ret << std::endl;
+        return 0;
+    }
+
+    std::cout << "here 17" << std::endl;
     return 0;
 }
 
@@ -121,7 +135,10 @@ int main() {
 
             snprintf(chars, 6, "%d", s);
 
+            std::cout << "here 1" << std::endl;
             (void) writeToCDev(chars, BRIGHTNESS);
+
+            std::cout << "here 100" << std::endl;
 
             std::this_thread::sleep_for(1000ms);
         }
