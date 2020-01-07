81d735c62ab9cd356ef853c6e1332ce03231bb8d
Sebastian Kaupper <kauppersebastian@gmail.com>
2020-01-07 12:09:15 +0100

    implement  ioctl commands for setting the pid and threshold

--- user/event_sensors/mpu9250.cpp
+++ user/event_sensors/mpu9250.cpp
@@ -32,8 +32,12 @@ using namespace std::placeholders;
 /* IO Control (IOCTL) */
 #define IOC_MODE_POLLING         0
 #define IOC_MODE_BUFFER          1
+#define IOC_SET_PID              2
+#define IOC_SET_THRESHOLD        3
 #define IOC_CMD_SET_READ_POLLING _IO(4711, IOC_MODE_POLLING)
 #define IOC_CMD_SET_READ_BUFFER  _IO(4711, IOC_MODE_BUFFER)
+#define IOC_CMD_SET_PID _IO(4711, IOC_SET_PID)
+#define IOC_CMD_SET_THRESHOLD _IOW(4711, IOC_SET_THRESHOLD, sizeof(uint32_t))
 
 
 /* Event specific defines */
@@ -57,6 +61,8 @@ struct MPU9250PollingPOD {
 } __attribute__((packed));
 
 struct MPU9250EventPOD {
+    uint32_t timestamp_lo[EVENT_PACKETS];
+    uint32_t timestamp_hi[EVENT_PACKETS];
     uint16_t acc_x[EVENT_PACKETS];
     uint16_t acc_y[EVENT_PACKETS];
     uint16_t acc_z[EVENT_PACKETS];
@@ -112,7 +118,6 @@ static void eventDataReady(int, siginfo_t *, void *) {
             MPU9250Data data = {};
             MPU9250PollingPOD tmp;
 
-            // TODO: do we need the remaining fields?
             tmp.acc_x  = pod.acc_x[i];
             tmp.acc_y  = pod.acc_y[i];
             tmp.acc_z  = pod.acc_z[i];
@@ -120,13 +125,17 @@ static void eventDataReady(int, siginfo_t *, void *) {
             tmp.gyro_y = pod.gyro_y[i];
             tmp.gyro_z = pod.gyro_z[i];
 
-            // std::cout << "Acc : x: " << std::hex << tmp.acc_x << ", y: " << std::hex << tmp.acc_y << ", z: " << std::hex << tmp.acc_z << std::endl;
-            // std::cout << "Gyro: x: " << std::hex << tmp.gyro_x << ", y: " << std::hex << tmp.gyro_y << ", z: " << std::hex << tmp.gyro_z << std::endl;
-            // std::cout << std::endl;
-
             data.event = true;
+            data.timeStamp = (((uint64_t) pod.timestamp_hi[i]) << 32) | pod.timestamp_lo[i];
             convertAccUnits(tmp, data);
             convertGyroUnits(tmp, data);
+
+            // std::cout << i << std::endl;
+            // std::cout << "Acc         : x: " << std::hex << tmp.acc_x << ", y: " << std::hex << tmp.acc_y << ", z: " << std::hex << tmp.acc_z << std::endl;
+            // std::cout << "Gyro        : x: " << std::hex << tmp.gyro_x << ", y: " << std::hex << tmp.gyro_y << ", z: " << std::hex << tmp.gyro_z << std::endl;
+            // std::cout << "Timestamp lo: " << pod.timestamp_lo[i] << std::endl;
+            // std::cout << "Timestamp hi: " << pod.timestamp_hi[i] << std::endl;
+            // std::cout << std::endl;
 
             eventData.emplace_back(std::move(data));
         }
@@ -161,6 +170,29 @@ std::string MPU9250::getTopic() const {
 MPU9250::MPU9250(double frequency) : StreamingSensor(frequency) {
     fSetEventQueue = std::bind(&MPU9250::setEventQueue, this, _1);
 
+
+    auto fd = fopen(CHARACTER_DEVICE.c_str(), "rb");
+    if (!fd) {
+        std::cerr << "Failed to open character device in signal handler" << std::endl;
+        return;
+    }
+
+    if (ioctl(fileno(fd), IOC_CMD_SET_PID) < 0) {
+        std::cerr << "Failed to set PID: " << strerror(errno) << std::endl;
+        fclose(fd);
+        return;
+    }
+
+    uint32_t threshold = 5000;
+    if (ioctl(fileno(fd), IOC_CMD_SET_THRESHOLD, &threshold) < 0) {
+        std::cerr << "Failed to set threshold: " << strerror(errno) << std::endl;
+        fclose(fd);
+        return;
+    }
+
+    fclose(fd);
+
+
     struct sigaction sig;
     sig.sa_sigaction = eventDataReady;
     sig.sa_flags     = SA_SIGINFO;
@@ -185,9 +217,18 @@ std::optional<MPU9250Data> MPU9250::doPoll() {
         return {};
     }
 
+    if (ioctl(fileno(fd), IOC_CMD_SET_READ_POLLING) < 0) {
+        std::cerr << "Requesting polling mode failed: " << strerror(errno) << std::endl;
+        std::cerr << "ATTENTION: The application invariants are broken now! An application restart is required!"
+                  << std::endl;
+        fclose(fd);
+        return {};
+    }
+
 
     if (fread(&pod, READ_SIZE, 1, fd) != 1) {
         std::cerr << "Failed to read sensor values" << std::endl;
+        fclose(fd);
         return {};
     }
 
