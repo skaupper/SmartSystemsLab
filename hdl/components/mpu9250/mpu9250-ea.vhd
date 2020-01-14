9b4b4383daa7e482925b49e721b977b2dddec3e6
Paul Götzinger <paul70079@gmail.com>
2020-01-14 20:02:20 +0100

    RTL for MPU9250 shock detection. (#14)

--- hdl/components/mpu9250/mpu9250-ea.vhd
+++ hdl/components/mpu9250/mpu9250-ea.vhd
@@ -13,13 +13,39 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 use work.Global.all;
 
--- 0 -> upper 16 bit: humidity, lower 16 bit: temperature
--- 1 -> lower word of timestamp
--- 2 -> upper word of timestamp
+-- Register Map
+-- - 0 -> Gyro X (16 Bit)
+-- - 1 -> Gyro Y (16 Bit)
+-- - 2 -> Gyro Z (16 Bit)
+-- - 3 -> Acc  X (16 Bit)
+-- - 4 -> Acc  Y (16 Bit)
+-- - 5 -> Acc  Z (16 Bit)
+-- - 6 -> Mag  X (16 Bit)
+-- - 7 -> Mag  Y (16 Bit)
+-- - 8 -> Mag  Z (16 Bit)
+-- - 9 -> lower word of timestamp
+-- - A -> upper word of timestamp
+-- - B -> Buffer CTRL + Data available
+--     - 0 -> Buffer 0 enable (cleared on every interrupt)
+--     - 1 -> Buffer 0 data available (read only)
+--     - 2 -> Buffer 1 enable (cleared on every interrupt)
+--     - 3 -> Buffer 1 data available (read only)
+--     - ...
+-- - C -> IEN (Interrupt enable)
+--     - 0 -> Buf0 Interrupt enable
+--     - 1 -> Buf1 Interrupt enable
+--     - ...
+-- - D -> ISR (Interrupt status register, write '1' to clear Interrupt)
+--     - 0 -> Buf0 Interrupt
+--     - 1 -> Buf1 Interrupt
+--     - ...
+-- - E -> select buffer to use
+-- - F -> Buffer data: reading alternates between x, y and z
 
 entity mpu9250 is
    generic (
-      gClkFrequency    : natural := 50E6);
+      gClkFrequency  : natural := 50E6;
+      gPreShockCount : natural := 256);
    port ( 
       iClk               : in  std_logic                     := '0';             -- clock.clk 
       inRst              : in  std_logic                     := '0';             -- reset.reset 
@@ -28,6 +54,7 @@ entity mpu9250 is
       avs_s0_readdata    : out std_logic_vector(31 downto 0);                    --       .readdata
       avs_s0_write       : in  std_logic                     := '0';             --       .write
       avs_s0_writedata   : in  std_logic_vector(31 downto 0) := (others => '0'); --       .writedata
+      irq_irq            : out std_logic;
       sclk, mosi         : out std_logic;
       miso               : in  std_logic;
       ss_n               : out std_logic_vector(0 downto 0);
@@ -36,15 +63,17 @@ entity mpu9250 is
 end entity mpu9250;
 
 architecture rtl of mpu9250 is
+
    constant cTimestampWidth : natural := 64;
    subtype aTimestamp is unsigned (cTimestampWidth-1 downto 0);
 
    constant cMsFrequency : natural := 1E3;
-   
+
    constant cSensorCount : natural := 9;
    constant cSensorValueWidth : natural := 16;
    subtype aSensorValue is std_ulogic_vector (cSensorValueWidth-1 downto 0);
-   type aSensorValues is array (0 to cSensorCount) of aSensorValue;
+   type aSensorValues is array (0 to cSensorCount-1) of aSensorValue;
+
 
    -- MPU Reg Addresses
    constant cMpuSmplrtDiv     : std_ulogic_vector(7 downto 0) := X"19"; -- 0x00
@@ -109,6 +138,11 @@ architecture rtl of mpu9250 is
    constant cAddrMagnetZ      : std_logic_vector(3 downto 0) := X"8";
    constant cAddrTsLo         : std_logic_vector(3 downto 0) := X"9";
    constant cAddrTsUp         : std_logic_vector(3 downto 0) := X"A";
+   constant cAddrCtrlAvail    : std_logic_vector(3 downto 0) := X"B";
+   constant cAddrIntEn        : std_logic_vector(3 downto 0) := X"C";
+   constant cAddrIntStatReg   : std_logic_vector(3 downto 0) := X"D";
+   constant cAddrShockLvl     : std_logic_vector(3 downto 0) := X"E";
+   constant cAddrBufData      : std_logic_vector(3 downto 0) := X"F";
 
    type aMpuCmd is record
       read : std_ulogic;
@@ -182,7 +216,56 @@ architecture rtl of mpu9250 is
       timestamp => (others => '0'),
       data      => (others => (others => '0')));
 
-   type aMpu9250State is (Init, InitMpu, InitMag, WaitData, ReadData);
+   type aRamCtrl is record
+      rAddr : unsigned(9 downto 0);
+      wAddr : unsigned(9 downto 0);
+      write : std_ulogic;
+   end record;
+
+   constant cRamCtrlClear : aRamCtrl := (
+      rAddr => (others => '0'),
+      wAddr => (others => '0'),
+      write => cInactivated);
+
+   type aRamState is (Idle, WaitShock, RecShock);
+   type aRamWriteState is (Idle, IncAddr);
+   type aRamReadState is (AccX,  AccY,  AccZ,
+                          GyroX, GyroY, GyroZ,
+                          MagX,  MagY,  MagZ,
+                          TimeL, TimeH);
+
+   function RamStateToULogic (s : aRamState) return std_ulogic is
+   begin
+      if s = Idle then
+         return cInactivated;
+      else
+         return cActivated;
+      end if;
+   end function;
+
+   type aRamReg is record
+      state  : aRamState;
+      wState : aRamWriteState;
+      rState : aRamReadState;
+      rInc   : std_ulogic;
+      intEn  : std_ulogic;
+      int    : std_ulogic;
+      avail  : std_ulogic;
+      ctrl   : aRamCtrl;
+   end record;
+
+   constant cRamRegClear : aRamReg := (
+      state  => Idle,
+      wState => Idle,
+      rState => AccX,
+      rInc   => cActivated,
+      intEn  => cInactivated,
+      int    => cInactivated,
+      avail  => cInactivated,
+      ctrl   => cRamCtrlClear);
+
+   type aMpu9250State is (Init, InitMpu, InitMag, WaitData,
+                          ReadData);
 
    constant cSpiFreq   : integer := 1E6;
    constant cSpiClkDiv : integer := integer(real(gClkFrequency)/real(2*cSpiFreq));
@@ -220,30 +303,38 @@ architecture rtl of mpu9250 is
       state => WriteAddr);
 
    type aRegSet is record
-      lock      : std_ulogic;
-      readdata  : std_ulogic_vector(31 downto 0);
-      reg       : aValueSet;
-      shadowReg : aValueSet;
-      timestamp : aTimestamp;
-      valid     : std_ulogic;
-      state     : aMpu9250State;
-      spi       : aSpiReg;
+      lock       : std_ulogic;
+      shockLevel : aSensorValue;
+      readdata   : std_ulogic_vector(31 downto 0);
+      reg        : aValueSet;
+      shadowReg  : aValueSet;
+      timestamp  : aTimestamp;
+      valid      : std_ulogic;
+      state      : aMpu9250State;
+      spi        : aSpiReg;
+      ram        : aRamReg;
+      newData    : std_ulogic;
    end record;
 
    constant cRegSetClear : aRegSet := (
-      lock      => cInactivated,
-      readdata  => (others => '0'),
-      reg       => cValueSetClear,
-      shadowReg => cValueSetClear,
-      timestamp => (others => '0'),
-      valid     => cInactivated,
-      state     => Init,
-      spi       => cSpiRegClear);
+      lock       => cInactivated,
+      shockLevel => std_ulogic_vector(to_signed(10E3, cSensorValueWidth)),
+      readdata   => (others => '0'),
+      reg        => cValueSetClear,
+      shadowReg  => cValueSetClear,
+      timestamp  => (others => '0'),
+      valid      => cInactivated,
+      state      => Init,
+      spi        => cSpiRegClear,
+      ram        => cRamRegClear,
+      newData    => cInactivated);
 
-   signal spiOut : aSpiOut;
-   signal nMpuInt, nMpuIntSync : std_ulogic;
+   signal spiOut   : aSpiOut;
    signal msTick   : std_ulogic;
    signal reg, nxR : aRegSet;
+   signal ramWData : std_ulogic_vector(255 downto 0);
+   signal ramRData : std_logic_vector(255 downto 0);
+   signal nMpuInt, nMpuIntSync : std_ulogic;
 begin
 
    strobe : entity work.StrobeGen
@@ -276,12 +367,25 @@ begin
       busy    => spiOut.busy,
       rx_data => spiOut.rxdata);
 
-   fsm : process( reg, avs_s0_read, msTick, avs_s0_address, nMpuIntSync, spiOut )
+   ram_inst : entity work.ram port map (
+      clock	    => iClk,
+      data      => std_logic_vector(ramWData),
+      rdaddress => std_logic_vector(reg.ram.ctrl.rAddr),
+      wraddress => std_logic_vector(reg.ram.ctrl.wAddr),
+      wren      => reg.ram.ctrl.write,
+      q         => ramRData);
+
+   fsm : process( reg, msTick, nMpuIntSync, spiOut, ramRData,
+                  avs_s0_read, avs_s0_address, avs_s0_write, avs_s0_writedata )
+      variable vRAddr : unsigned(9 downto 0);
    begin
       nxR           <= reg;
       nxR.readdata  <= (others => '0');
       nxR.valid     <= cActivated;
+      nxR.newData   <= cInactivated;
       nxR.spi.input <= cSpiInClear;
+      nxR.ram.ctrl.write <= cInactivated;
+      nxR.ram.rInc  <= cActivated;
 
       -- Timestamp logic
       if msTick = cActivated then
@@ -420,13 +524,13 @@ begin
                         nxR.shadowreg.data(reg.spi.idx)( 7 downto 0) <= std_ulogic_vector(spiOut.rxdata);
 
                         if reg.spi.idx = cMpuRead'high then
-                           nxR.state     <= WaitData;
+                           nxR.newData <= cActivated;
+                           nxR.state   <= WaitData;
                         else
                            nxR.spi.idx   <= reg.spi.idx + 1;
                            nxR.spi.state <= WriteAddr;
                         end if;
                      end if;
-
                   end if;
                when others =>
                   nxR.state <= Init;   --ERROR
@@ -435,7 +539,53 @@ begin
             nxR.state <= Init;
       end case;
 
-      -- Bus logic
+      case reg.ram.state is
+         when Idle =>
+            nxR.ram.wState <= Idle;
+         when WaitShock =>
+            case reg.ram.wState is
+               when Idle =>
+                  if reg.newData = cActivated then
+                     nxR.ram.ctrl.write <= cActivated;
+                     nxR.ram.wState     <= IncAddr;
+                  end if;
+               when IncAddr =>
+                  nxR.ram.ctrl.wAddr <= reg.ram.ctrl.wAddr + 1;
+                  nxR.ram.wState     <= Idle;
+
+                  if abs(signed(reg.shadowReg.data(3))) > signed(reg.shockLevel) OR
+                     abs(signed(reg.shadowReg.data(4))) > signed(reg.shockLevel) OR
+                     abs(signed(reg.shadowReg.data(5))) > signed(reg.shockLevel) then
+
+                     nxR.ram.state      <= RecShock;
+                     nxR.ram.ctrl.rAddr <= reg.ram.ctrl.wAddr - gPreShockCount;
+                  end if;
+               when others =>
+                  nxR.ram.state <= Idle;
+            end case;
+         when RecShock =>
+            case reg.ram.wState is
+               when Idle =>
+                  if reg.ram.ctrl.wAddr = reg.ram.ctrl.rAddr then
+                     nxR.ram.state  <= Idle;
+                     nxR.ram.int    <= cActivated;
+                     nxR.ram.avail  <= cActivated;
+                  elsif reg.newData = cActivated then
+                     nxR.ram.ctrl.write <= cActivated;
+                     nxR.ram.wState     <= IncAddr;
+                     nxR.ram.rState     <= AccX;
+                  end if;
+               when IncAddr =>
+                  nxR.ram.ctrl.wAddr <= reg.ram.ctrl.wAddr + 1;
+                  nxR.ram.wState     <= Idle;
+               when others =>
+                  nxR.ram.state <= Idle;
+            end case;
+         when others =>
+            nxR.ram.state <= Idle;
+      end case;
+
+      -- Bus logic - read
       if avs_s0_read = cActivated then
          case( avs_s0_address ) is
             when cAddrGyroX =>
@@ -482,9 +632,93 @@ begin
                nxR.readdata <= std_ulogic_vector(reg.reg.timestamp(63 downto 32));
                nxR.lock     <= cInactivated;
 
+            when cAddrCtrlAvail =>
+               nxR.readdata(0) <= RamStateToULogic(reg.ram.state);
+               nxR.readdata(1) <= reg.ram.avail;
+
+            when cAddrIntEn =>
+               nxR.readdata(0) <= reg.ram.intEn;
+
+            when cAddrIntStatReg =>
+               nxR.readdata(0) <= reg.ram.int;
+
+            when cAddrShockLvl =>
+               nxR.readdata(15 downto 0) <= reg.shockLevel;
+
+            when cAddrBufData =>
+               if reg.ram.avail = cActivated AND reg.ram.rInc = cActivated then
+                  nxR.ram.rInc <= cInactivated;
+                  case reg.ram.rState is
+                     when AccX =>
+                        nxR.readdata(aSensorValue'range) <= std_ulogic_vector(ramRData( 15 downto   0));
+                        nxR.ram.rState <= AccY;
+                     when AccY =>
+                        nxR.readdata(aSensorValue'range) <= std_ulogic_vector(ramRData( 31 downto  16));
+                        nxR.ram.rState <= AccZ;
+                     when AccZ =>
+                        nxR.readdata(aSensorValue'range) <= std_ulogic_vector(ramRData( 47 downto  32));
+                        nxR.ram.rState <= GyroX;
+                     when GyroX =>
+                        nxR.readdata(aSensorValue'range) <= std_ulogic_vector(ramRData( 79 downto  64));
+                        nxR.ram.rState <= GyroY;
+                     when GyroY =>
+                        nxR.readdata(aSensorValue'range) <= std_ulogic_vector(ramRData( 95 downto  80));
+                        nxR.ram.rState <= GyroZ;
+                     when GyroZ =>
+                        nxR.readdata(aSensorValue'range) <= std_ulogic_vector(ramRData(111 downto  96));
+                        nxR.ram.rState <= MagX;
+                     when MagX =>
+                        nxR.readdata(aSensorValue'range) <= std_ulogic_vector(ramRData(143 downto 128));
+                        nxR.ram.rState <= MagY;
+                     when MagY =>
+                        nxR.readdata(aSensorValue'range) <= std_ulogic_vector(ramRData(159 downto 144));
+                        nxR.ram.rState <= MagZ;
+                     when MagZ =>
+                        nxR.readdata(aSensorValue'range) <= std_ulogic_vector(ramRData(175 downto 160));
+                        nxR.ram.rState <= TimeL;
+                     when TimeL =>
+                        nxR.readdata <= std_ulogic_vector(ramRData(223 downto 192));
+                        nxR.ram.rState <= TimeH;
+                     when TimeH =>
+                        nxR.readdata <= std_ulogic_vector(ramRData(255 downto 224));
+                        nxR.ram.rState <= AccX;
+                        vRAddr := reg.ram.ctrl.rAddr + 1;
+                        if vRAddr = reg.ram.ctrl.wAddr then
+                           nxR.ram.avail <= cInactivated;
+                        else
+                           nxR.ram.ctrl.rAddr <= vRAddr;
+                        end if;  
+                     when others =>
+                        null;
+                  end case;
+               else
+                  nxR.readdata <= X"DEADBEEF";  -- Invalid read
+               end if;
+
+            when others =>
+               nxR.readdata <= X"DEADCE11";  -- Invalid read
+         end case ;
+      end if;
+
+      -- Bus logic - write
+      if avs_s0_write = cActivated then
+         case avs_s0_address is
+            when cAddrCtrlAvail =>
+               if avs_s0_writedata(0) = cActivated then
+                  nxR.ram.state <= WaitShock;
+               end if;
+            when cAddrIntEn =>
+               nxR.ram.intEn <= std_ulogic(avs_s0_writedata(0));
+            when cAddrIntStatReg =>
+               if avs_s0_writedata(0) = cActivated then
+                  nxR.ram.int   <= cInactivated;
+               end if;
+            when cAddrShockLvl =>
+               nxR.shockLevel <= std_ulogic_vector(avs_s0_writedata(15 downto 0));
+         
             when others =>
                null;
-         end case ;
+         end case;
       end if;
    end process; -- fsm
 
@@ -508,6 +742,21 @@ begin
       end if;
    end process ; -- regProc
 
+   ramWData(255 downto 192) <= std_ulogic_vector(reg.shadowReg.timestamp);
+   ramWData(191 downto 176) <= X"DEA2";
+   ramWData(175 downto 160) <= reg.shadowReg.data(8); -- Mag Z
+   ramWData(159 downto 144) <= reg.shadowReg.data(7); -- Mag Y
+   ramWData(143 downto 128) <= reg.shadowReg.data(6); -- Mag X
+   ramWData(127 downto 112) <= X"DEA1";
+   ramWData(111 downto  96) <= reg.shadowReg.data(2); -- Gyro Z
+   ramWData( 95 downto  80) <= reg.shadowReg.data(1); -- Gyro Y
+   ramWData( 79 downto  64) <= reg.shadowReg.data(0); -- Gyro X
+   ramWData( 63 downto  48) <= X"DEA0";
+   ramWData( 47 downto  32) <= reg.shadowReg.data(5); -- Acc Z
+   ramWData( 31 downto  16) <= reg.shadowReg.data(4); -- Acc Y
+   ramWData( 15 downto   0) <= reg.shadowReg.data(3); -- Acc X
+
+   irq_irq <= reg.ram.intEn AND reg.ram.int;
    avs_s0_readdata <= std_logic_vector(reg.readdata);
 
 end architecture rtl; -- of new_component
--- hdl/scripts/adaptDTS.sh
+++ hdl/scripts/adaptDTS.sh
@@ -13,19 +13,17 @@ cp ./input/overlaytemplate.dtso ${OUTPUT_DTSO}
 
 chmod 777 ${OUTPUT_DTSO}
 
-
 # get peripherals inside bridge
 PERIPHERALS="$(perl -0777 -ne 'print $1 if /(?:hps_0_bridges:)(?:.+?ranges.+?;)(.*?)(?:}; \/\/end bridge)/sp' "${OUTPUT_DTS}")"
 echo "$PERIPHERALS" > ".tmp"
-
-# fix interrupt flag settings
-# see platform_get_irq https://patchwork.kernel.org/patch/9264913/
-perl -i -p -e 's/(interrupts.*<)(.*? .*? )(.*?)(>;)/$1$2\Q0\E$4$5/g' ".tmp"
 
 # fill template with peripherals
 perl -i -p -e '$text=`cat .tmp`; s/INSERTIONMARK/$text/g' ${OUTPUT_DTSO}
 
 # strip bridge peripherals
 sed -i -E "/hps_0_bridges/,/\(hps_0_bridges\)/d" ${OUTPUT_DTS}
+
+#https://lore.kernel.org/patchwork/patch/863149/
+sed -E -i -e "s/@0x([0-9a-fA-F\.]+)\s?\{/@\L\1 \{/g" -e "s/@0+([0-9a-fA-F\.]+)\s?\{/@\L\1 \{/g" ${OUTPUT_DTSO}
 
 echo "Leaving file $0 ---------------------------------------------------"
